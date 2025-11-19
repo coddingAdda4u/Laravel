1fd7e8f8f8cc47bfb1e7bad3c6d314c0d9dac24d
coddingAdda4u <omkashyap6870@gmail.com>
2025-11-19 14:30:35 +0530

    formating files

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,49 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')->group(function () {
     // Home Page
-    Route::get('/', fn() => view('pages.home'))->name('home');
+    Route::get(
+        '/',
+        fn() =>
+        view('pages.home')
+    )->name('home');
     // About Page
-    Route::get('about', fn() => view('pages.about'))->name('about');
+    Route::get(
+        'about',
+        fn() =>
+        view('pages.about')
+    )->name('about');
     // Contact Page
-    Route::get('contact', fn() => view('pages.contact'))->name('contact');
+    Route::get(
+        'contact',
+        fn() =>
+        view('pages.contact')
+    )->name('contact');
     // Gallery Page
-    Route::get('gallery', fn() => view('pages.gallery'))->name('gallery');
+    Route::get(
+        'gallery',
+        fn() =>
+        view('pages.gallery')
+    )->name('gallery');
     // Welcome Page
-    Route::get('welcome', fn() => view('welcome'))->name('welcome');
-// Redirect Route
-    Route::get('redirect', fn() => view('pages.redirect'))->name('redirect');
+    Route::get(
+        'welcome',
+        fn() =>
+        view('welcome')
+    )->name('welcome');
+    // Redirect Page
+    Route::get(
+        'redirect',
+        fn() =>
+        view('pages.redirect')
+    )->name('redirect');
 
 });
+
+// Login Redirect Route
 Route::redirect('/login', '/redirect')->name('login');
-Route::fallback(fn() => view('pages.fallback'))->name('fallback');
+
+// Fallback Route
+Route::fallback(
+    fn() =>
+    view('pages.fallback')
+)->name('fallback');
